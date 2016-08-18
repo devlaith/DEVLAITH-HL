34147f499312915204cfbfe544c62735cbee1a1a
devlaith <allame277@gmail.com>
2016-08-18 14:15:37 +0430

    Update addsudo.lua

--- plugins/addsudo.lua
+++ plugins/addsudo.lua
@@ -1,17 +1,40 @@
---[[ 
-▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
-▀▄ ▄▀                                      ▀▄ ▄▀ 
-▀▄ ▄▀    BY Th3_BOOS                   ▀▄ ▄▀ 
-▀▄ ▄▀     BY Th3_BOOS (@Th3_BOOS)    ▀▄ ▄▀ 
-▀▄ ▄▀ JUST WRITED BY Th3_BOOS          ▀▄ ▄▀   
-▀▄ ▄▀     ADD SUDO  : اضافه مطور           ▀▄ ▄▀ 
-▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
---]]
 do
 
 local function callback(extra, success, result)
   vardump(success)
   vardump(result)
+end
+
+local function run(msg, matches)
+ if matches[1] == 'اضف مطور' then
+        chat = 'chat#'..msg.to.id
+        user1 = 'user#'..155713097
+        chat_add_user(chat, user1, callback, false)
+	return "تم اضافة المطور الى هذه المجموعة"
+      end
+if matches[1] == 'اضف مطور' then
+        chat = 'chat#'..msg.to.id
+        user2 = 'user#'..155713097
+        chat_add_user(chat, user2, callback, false)
+	return "تم اضافة المطور الى هذه المجموعة"
+      end
+ 
+ end
+
+return {
+  description = "Invite Sudo and Admin", 
+  usage = {
+    "/addsudo : invite Bot Sudo", 
+	},
+  patterns = {
+    "^(اضف مطور)",
+    "^(اضف مطور)",
+    "^(add dev)",
+    "^(add dev)",
+  }, 
+  run = run,
+}
+
 end
 
 local function run(msg, matches)
